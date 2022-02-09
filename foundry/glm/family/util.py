2ff265dbab7f2ba6e3793647e93abde8dd8d32ac
Jacob <jacob.dink@strong.io>
2022-02-09 12:16:47 -0600

    Cache calls to inspect.signature

--- foundry/glm/family/util.py
+++ foundry/glm/family/util.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Union
+from typing import Union, Type
 
 import torch
 from foundry.util import ArrayType
@@ -19,6 +19,28 @@ def log1mexp(x: torch.Tensor) -> torch.Tensor:
     return out.to(dtype=out_dtype)
 
 
+_dist2pars = {}
+
+
+def _get_dist_pars(cls: Type[torch.distributions.Distribution]) -> list:
+    if cls not in _dist2pars:
+        # check that default approach is OK by making sure it captures __init__ kwargs:
+        init_kwargs = set(
+            p.name
+            for p in inspect.signature(cls.__init__).parameters.values()
+            if p.name not in ("self", "validate_args")
+        )
+        non_params = init_kwargs - set(cls.arg_constraints)
+        if non_params:
+            raise TypeError(
+                f"Unable to subset {cls.__name__} using default approach, due to params in init that aren't in "
+                f"`arg_constraints`: {non_params}. Please report this error to the package maintainer."
+            )
+
+        _dist2pars[cls] = list(cls.arg_constraints)
+    return _dist2pars[cls]
+
+
 def subset_distribution(
         dist: torch.distributions.Distribution,
         item: Union[slice, ArrayType]
@@ -27,21 +49,8 @@ def subset_distribution(
     if hasattr(dist, '__getitem__'):
         return dist[item]
 
-    # check that default approach is OK by making sure it captures __init__ kwargs:
-    init_kwargs = set(
-        p.name
-        for p in inspect.signature(dist.__init__).parameters.values()
-        if p.name not in ("self", "validate_args")
-    )
-    non_param = init_kwargs - set(dist.arg_constraints)
-    if non_param:
-        raise TypeError(
-            f"Unable to subset {type(dist).__name__} using default approach, due to params in init that aren't in "
-            f"`arg_constraints`: {non_param}. Please report this error to the package maintainer."
-        )
-
     # approach in torch.distributions.Distribution:
-    param_names = [k for k in list(dist.arg_constraints) if k in dist.__dict__]
+    param_names = [k for k in _get_dist_pars(dist.__class__) if k in dist.__dict__]
     new_kwargs = {par: getattr(dist, par)[item] for par in param_names}
     new = type(dist)(**new_kwargs)
     if '_validate_args' in dist.__dict__:
