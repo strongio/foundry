56407c5343afe9ba44fe8a78fa508be43f1ca0ff
Jacob <jacob.dink@strong.io>
2023-01-03 10:34:39 -0600

    wip mc likelihood

--- foundry/glm/hlm.py
+++ foundry/glm/hlm.py
@@ -35,10 +35,8 @@ class Covariance:
 
 def log_chol_to_chol(log_diag: torch.Tensor, off_diag: torch.Tensor) -> torch.Tensor:
     assert log_diag.shape[:-1] == off_diag.shape[:-1]
-
     rank = log_diag.shape[-1]
     L1 = torch.diag_embed(torch.exp(log_diag))
-
     L2 = torch.zeros_like(L1)
     mask = torch.tril_indices(rank, rank, offset=-1)
     L2[mask[0], mask[1]] = off_diag
@@ -115,7 +113,7 @@ class Hlm(Glm):
         if not isinstance(X, pd.DataFrame):
             raise NotImplementedError("Currently only DataFrame X supported")
         # [dist_param, *gfs, group_ids]
-        col_mapping = {self.family.params[0]: self.fixeffs}
+        col_mapping = {self.family.loc_param: self.fixeffs}
         col_mapping.update(self.raneff_design)
         assert 'group_ids' not in col_mapping
         col_mapping['group_ids'] = list(self.raneff_design)
@@ -129,26 +127,25 @@ class Hlm(Glm):
                             raneff_design: Dict[str, Sequence[Union[str, callable]]],
                             verbose: bool = True
                             ) -> Tuple[Sequence[str], Dict[str, Sequence[str]]]:
-        any_callables = False
-        standarized_cols = {}
+        full_design = raneff_design.copy()
         assert 'fixeffs' not in raneff_design
-        for cat, to_standardize in {**raneff_design, **{'fixeffs': fixeffs}}.items():
-            if cat not in standarized_cols:
-                standarized_cols[cat] = []
+        full_design['fixeffs'] = fixeffs
+
+        _any_callables = False
+        standarized_cols = {cat: [] for cat in full_design}
+        for cat, to_standardize in full_design.items():
             for i, cols in enumerate(to_standardize):
                 if callable(cols):
-                    any_callables = True
+                    _any_callables = True
                     cols = cols(X)
                     if not cols:
                         warn(f"Element {i} of {cat} was a callable that, when given X, returned no rows.")
                     # (1) callables might have overlapping matches, drop dupes, (2) don't include grouping factor
                     cols = [c for c in cols if c not in standarized_cols[cat] and c not in raneff_design]
-                if isinstance(cols, str):
-                    if cols not in X.columns:
-                        raise RuntimeError(f"No column named {cols}")
-                    cols = [cols]
+                elif isinstance(cols, str):
+                    raise RuntimeError("`cols` must be sequence of strings, not a single string.")
                 standarized_cols[cat].extend(cols)
-        if any_callables and verbose:
+        if _any_callables and verbose:
             print(f"Model-features: {standarized_cols}")
         fixeffs = standarized_cols.pop('fixeffs')
         return fixeffs, standarized_cols
@@ -167,7 +164,7 @@ class Hlm(Glm):
                     low_rank = gf_cov.replace('low_rank', '').lstrip('_')
                     low_rank = int(low_rank) if low_rank.isdigit() else int(np.sqrt(len(gf_preds)))
                 elif gf_cov != 'full_rank':
-                    raise ValueError("If covariance is a string, expect 'full_rank' or 'low_rank{rank}'")
+                    raise ValueError("If covariance is a string, expected 'full_rank' or 'low_rank{rank}'")
                 gf_cov = Covariance(low_rank=low_rank)
             raneff_covariance[gf] = gf_cov
 
@@ -185,6 +182,7 @@ class Hlm(Glm):
                 raise NotImplementedError
 
             # add random-effects parameters:
+            params_and_links = params_and_links.copy()
             for gf, cov in self.raneff_covariance.items():
                 assert gf not in params_and_links
                 params_and_links[f're_cov_{gf}'] = cov
@@ -201,54 +199,101 @@ class HlmFamily(Family):
     def __init__(self,
                  distribution_cls: Type[torch.distributions.Distribution],
                  params_and_links: Dict[str, callable],
-                 is_classifier: Optional[bool] = None,
-                 mcmc_likelihood: Optional[bool] = None):
+                 supports_predict_proba: Optional[bool] = None,
+                 mc_likelihood: Union[int, bool, None] = None):
         # TODO: what about composed multivariate?
 
         if issubclass(distribution_cls, distributions.Normal):
-            if mcmc_likelihood is None:
-                mcmc_likelihood = True
+            if mc_likelihood is None:
+                mc_likelihood = True
         else:
-            if mcmc_likelihood is not None:
-                raise NotImplementedError(f"Must use mcmc_likelihood for {distribution_cls.__name__}")
+            if mc_likelihood is not None:
+                raise NotImplementedError(f"Must use ``mc_likelihood`` for {distribution_cls.__name__}")
 
-        self.mcmc_likelihood = mcmc_likelihood
+        self.mc_likelihood = mc_likelihood
+        self._mc_white_noise = {}
 
         super().__init__(
             distribution_cls=distribution_cls,
             params_and_links=params_and_links,
-            is_classifier=is_classifier
+            supports_predict_proba=supports_predict_proba
         )
 
-    def __call__(self, **kwargs) -> torch.distributions.Distribution:
-        # the usual:
-        dist_kwargs = {}
-        for p, ilink in self.params_and_links.items():
-            dist_kwargs[p] = ilink(kwargs.pop(p))
+        self.loc_param = self.params[0]
 
-        # ok so now dist_kwargs should have [loc, *re_cov_{gfs}, optional[scale]]
-        # and kwargs should have [{gfs}, group_ids].
-        # 1. for mcmc:
-        #    a. for one grouping factor: need to sim betas for each gf from each re_cov_gf, return mvnorm of sims
-        #    b. for >1 grouping factor: one giant mvnorm
-        # 2. for closed_form:
-        #    a. for one grouping factor: one mvnorm with num_groups batch-dim
-        #    b. for >1 grouping factor: one giant mvnorm
-        raise NotImplementedError
-        # dist_kwargs.update(kwargs)
-        # return self.distribution_cls(**dist_kwargs)
+    def get_mc_white_noise(self, grouping_factor: str, num_sims: int, num_res: int, **kwargs) -> torch.Tensor:
+        shape = (num_sims, num_res)
+        if grouping_factor in self._mc_white_noise:
+            assert tuple(self._mc_white_noise[grouping_factor].shape) == shape
+        else:
+            self._mc_white_noise[grouping_factor] = torch.randn(shape, **kwargs)
+        return self._mc_white_noise[grouping_factor]
+
+    def __call__(self, **kwargs) -> torch.distributions.Distribution:
+        cov_params = {}
+        raw_dist_params = {}
+        for nm, ilink in self.params_and_links.items():
+            if nm.startswith('re_cov_'):
+                cov_params[nm.replace('re_cov_', '')] = ilink(kwargs.pop(nm))
+            else:
+                raw_dist_params[nm] = kwargs.pop(nm)
+
+        # XXX
+        re_model_mats = {k:mm for k, mm in kwargs.items() if k!='group_ids'}
+        gf_names = list(re_model_mats)
+        re_distributions = {
+            nm: torch.distributions.MultivariateNormal(loc=0, covariance_matrix=cov_params[nm])
+            for nm in gf_names
+        }
+
+        if self.mc_likelihood:
+            num_sims = self.mc_likelihood if isinstance(self.mc_likelihood, int) else 500
+            #    a. for one grouping factor: need to sim betas for each gf from each re_cov_gf, return mvnorm of sims
+            if len(re_model_mats) == 1:
+                Xgf = re_model_mats[gf_names[0]]
+                Xgf = torch.cat([torch.ones_like(Xgf[:, 0]), Xgf], 1)  # TODO: intercept-only
+                white_noise = self.get_mc_white_noise(
+                    grouping_factor=gf_names[0],
+                    num_sims=num_sims,
+                    num_res=Xgf.shape[-1]
+                )
+                re_samples = (re_distributions[gf_names[0]].scale_tril @ white_noise.unsqueeze(-1)).squeeze(-1)
+                yhat_r_samples = (Xgf.unsqueeze(-1) * re_samples.T.unsqueeze(0)).sum(1)
+                # y_dist = torch.distributions.Normal(loc=yhat_samples, scale=self.residual_var ** .5)
+                # integral_wrt_b_random[ p(y|b_fixed, b_random)] * p(b_random|re_cov) ]
+                #   ~=
+                # mean[ p(y|b_fixed, b_random_i)) ], b_random_i ~ mvnorm(re_cov)
+                # log_integrand = y_dist.log_prob(y.unsqueeze(-1))
+                dist_kwargs = {}
+                for nm, raw_param in raw_dist_params.items():
+                    raw_param = raw_param.unsqueeze(-1)
+                    if nm == self.loc_param:
+                        raw_param = raw_param + yhat_r_samples
+                    ilink = self.params_and_links[nm]
+                    dist_kwargs[nm] = ilink(raw_param)
+                return self.distribution_cls(**dist_kwargs)
+            else:
+                #    b. for >1 grouping factor: one giant mvnorm
+                raise NotImplementedError
+        else:
+            #    a. for one grouping factor: one mvnorm with num_groups batch-dim
+            #    b. for >1 grouping factor: one giant mvnorm
+            pass
 
     def log_prob(self,
                  distribution: torch.distributions.Distribution,
                  value: torch.Tensor,
-                 weight: Optional[torch.Tensor] = None) -> torch.Tensor:
-        value, weight = self._validate_values(value, weight, distribution)
-        # CHALLENGE: do we need to get group_ids back?
-
-        # log_probs = distribution.log_prob(value)
+                 weight: Optional[torch.Tensor] = None,
+                 group_ids: pd.DataFrame = None) -> torch.Tensor:
+        if weight is not None:
+            # TODO
+            raise NotImplementedError
+        if self.mc_likelihood:
+            log_probs = distribution.log_prob(value)
+            ngroups = cache['group_ids_seq'].max() + 1
+            group_ids_broad = cache['group_ids_seq'].expand(-1, nsim)
+            lps_per_group = torch.zeros((ngroups, nsim)).scatter_add(0, group_ids_broad, log_integrand)
+            log_probs_unnorm = lps_per_group.logsumexp(1)
+            log_probs = log_probs_unnorm - math.log(nsim)
         # log_probs = weight * log_probs
         # return log_probs
-
-    @staticmethod
-    def log_cdf(distribution: distributions.Distribution, value: torch.Tensor, lower_tail: bool) -> torch.Tensor:
-        raise NotImplementedError  # TODO
