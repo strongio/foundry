09bbb519bec50db6928a73983dee04cafe7f397b
Jacob <jacob.dink@strong.io>
2023-01-06 10:53:57 -0600

    Update __init__.py

--- foundry/preprocessing/__init__.py
+++ foundry/preprocessing/__init__.py
@@ -1,10 +1,11 @@
 from .sklearn import (
     DataFrameTransformer,
     InteractionFeatures,
+    ColumnDropper,
+    ToCategorical,
     as_transformer,
     identity,
     make_column_selector,
-    ColumnDropper,
     make_drop_transformer
 )
 from .dates import FourierFeatures
