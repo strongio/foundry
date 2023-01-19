b1d36592946c28761df318f3c94380ddabe82d4b
Jacob <jacob.dink@strong.io>
2023-01-19 11:57:16 -0600

    fix family=bernouilli; add test

--- tests/glm/test_glm.py
+++ tests/glm/test_glm.py
@@ -40,6 +40,17 @@ def test_multi_output_integration(family: str):
         np.testing.assert_allclose(glm.predict(X).mean(0), y.mean(), atol=.001)
 
 
+@pytest.mark.parametrize(
+    argnames=['family'],
+    argvalues=[('categorical',), ('bernoulli',)]
+)
+def test_classification_integration(family: str):
+    y = pd.Series([0] * 3 + [1] * 2, name='cat').to_frame()
+    glm = Glm(family=family)
+    X = pd.DataFrame(index=y.index)
+    glm.fit(X=X, y=y, verbose=False)
+
+
 class _FakeDist:
     arg_constraints = {
         'param1': constraints.real,
