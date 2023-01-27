256ccf15b7a09ee43f1443f63b8d62417aec1e26
Jacob <jacob.dink@strong.io>
2023-01-27 08:54:57 -0600

    update test

--- tests/glm/distribution/test_family.py
+++ tests/glm/distribution/test_family.py
@@ -9,6 +9,7 @@ import pytest
 import torch.distributions
 
 from foundry.glm import Glm
+from foundry.glm.distributions import NegativeBinomial
 from foundry.glm.family import Family
 from foundry.glm.glm import family_from_string
 from foundry.glm.util import SoftmaxKp1
@@ -54,14 +55,14 @@ class TestFamily:
             Params(
                 alias='negative_binomial',
                 call_input={
-                    'probs': torch.tensor([0., 1.]).unsqueeze(-1),
-                    'total_count': torch.tensor([0., 1.]).unsqueeze(-1)
+                    'loc': torch.tensor([0., 0.]).unsqueeze(-1),
+                    'dispersion': torch.tensor([0., 1.]).unsqueeze(-1)
                 },
                 expected_supports_predict_proba=False,
-                expected_has_total_count=True,
-                expected_call_output=torch.distributions.NegativeBinomial(
-                    probs=torch.tensor([.5, 0.7311]).unsqueeze(-1),
-                    total_count=torch.tensor([1., math.e]).unsqueeze(-1)
+                expected_has_total_count=False,
+                expected_call_output=NegativeBinomial(
+                    loc=torch.tensor([1., 1.]).unsqueeze(-1),
+                    dispersion=torch.tensor([1., math.e]).unsqueeze(-1)
                 ),
             ),
             Params(
--- tests/glm/distribution/util.py
+++ tests/glm/distribution/util.py
@@ -4,6 +4,12 @@ from tests.conftest import assert_tensors_equal
 
 
 def assert_dist_equal(x: torch.distributions.Distribution, y: torch.distributions.Distribution, tol: float = .001):
+    if type(x) != type(y):
+        if type(x).__name__ == type(y).__name__ and x.__module__ != y.__module__:
+            # this is legit confusing
+            raise AssertionError(f"type(x) ({x.__module__}) != type(y) ({y.__module__})")
+        assert type(x) == type(y)  # otherwise not too confusing
+
     assert x.arg_constraints == y.arg_constraints
     for nm in x.arg_constraints:
         x_attr = getattr(x, nm)
