30edbfb61db7b3b8e8d8a654d3b51a9a29a7ad73
Jacob <jacob.dink@strong.io>
2023-11-22 14:26:53 -0600

    add simulate_grouped_data

--- foundry/data.py
+++ foundry/data.py
@@ -7,6 +7,7 @@ from scipy import special
 from sklearn.datasets import make_regression
 from sklearn.preprocessing import quantile_transform
 
+from foundry.covariance import log_chol_to_chol
 from foundry.glm.family import Family
 from foundry.glm.glm import family_from_string
 
@@ -221,3 +222,115 @@ def simulate_data(family: str,
     out.attrs['ground_truth_linpred'] = {p: getattr(distribution, p) for p in family.params}
 
     return out
+
+
+def simulate_grouped_data(family: str,
+                          n_groups: int,
+                          n_samples_per_group: int,
+                          n_features: int,
+                          bias: float = 0,
+                          std_multi: Optional[np.ndarray] = None,
+                          **kwargs) -> pd.DataFrame:
+    """
+    :param family: A family-alias, see ``Glm.family_names``.
+    :param n_groups: Number of groups
+    :param n_samples_per_group: Number of observations per group
+    :param n_features: A number > 0 of random-effects, with the first being the intercept.
+    :param bias: The bias aka intercept.
+    :param std_multi: Multiplier for the standard-deviation of the random effects. Defaults to greater variance for the
+    intercept than the predictors.
+    :return: A dataframe with predictors in format ``x_{i}``, group-id ``group_id``, and target ``target``. The
+     ground-truth predictor-coefficients are stored in the ``attrs`` attribute of the dataframe.
+    """
+    assert n_features >= 0
+    n_raneffs = n_features + 1
+
+    if isinstance(family, str):
+        family = family_from_string(family)
+
+    # set torch random-state:
+    random_state = kwargs.get('random_state') or np.random.randint(1e6)
+    if isinstance(random_state, int):
+        random_state = np.random.RandomState(random_state)
+
+    # generate random-effects covariance matrix. if >=10 features, generate a low-ranked covariance matrix:
+    if n_raneffs < 10:
+        rf_cov = _random_cov_mat(n_raneffs, eps=0.1, random_state=random_state)
+    else:
+        lr = random_state.randn(n_raneffs, int(np.sqrt(n_raneffs)))
+        rf_cov = lr @ lr.T + .1 * np.eye(n_raneffs)
+
+    # incorporate std_multi:
+    if std_multi is None:
+        std_multi = [0.25] + [0.1] * (n_raneffs - 1)
+    if isinstance(std_multi, (int, float)):
+        std_multi = [std_multi] * n_raneffs
+    std_multi = np.diag(std_multi)
+    rf_cov = std_multi @ rf_cov @ std_multi
+
+    # generate random-effects:
+    ground_truth_coefs = random_state.normal(size=n_raneffs)
+    ground_truth_coefs[0] = bias
+    raneffects = random_state.multivariate_normal(mean=ground_truth_coefs, cov=rf_cov, size=n_groups)
+
+    # generate model-mat
+    n_samples = n_groups * n_samples_per_group
+    feature_names = [f'x{i + 1}' for i in range(n_features)]
+    if feature_names:
+        X = random_state.multivariate_normal(
+            mean=np.zeros(n_features),
+            cov=_random_corr_mat(n_features, eps=np.sqrt(n_features), random_state=random_state) / np.sqrt(n_features),
+            size=n_samples
+        )
+        out = pd.DataFrame(X, columns=feature_names)
+        Xi = np.concatenate([np.ones((len(X), 1)), X], 1)  # intercept
+    else:
+        out = pd.DataFrame(index=range(n_samples))
+        Xi = np.ones((n_samples, 1))
+
+    # group indices:
+    group_idx = []
+    for group_id in range(n_groups):
+        group_idx.extend([group_id] * n_samples_per_group)
+    group_idx = np.asanyarray(group_idx)
+    out['group_id'] = group_idx
+
+    # generate target:
+    y_linpred = (raneffects[group_idx] * Xi).sum(1)
+    family_kwargs = {family.params[0]: torch.as_tensor(y_linpred)}
+    for par in family.params[1:]:
+        family_kwargs[par] = torch.as_tensor(kwargs.get(par, 0.0))
+    if 'total_count' in family_kwargs:
+        out['total_count'] = family_kwargs['total_count']
+    distribution = family(**family_kwargs)
+    with torch.random.fork_rng():
+        torch.manual_seed(hash(random_state))
+        out['target'] = distribution.sample().numpy()
+
+    # save ground-truth:
+    out.attrs['ground_truth_coefs'] = {family.params[0]: ground_truth_coefs}
+    out.attrs['ground_truth_linpred'] = {family.params[0]: y_linpred}
+    out.attrs['ground_truth_raneffs'] = pd.DataFrame(
+        raneffects - ground_truth_coefs[None, :],
+        columns=['Intercept'] + feature_names
+    )
+    return out
+
+
+def _random_cov_mat(rank: int,
+                    eps: float = .001,
+                    random_state: Optional[np.random.RandomState] = None) -> np.ndarray:
+    if random_state is None:
+        random_state = np.random
+    L_log_diag = random_state.randn(rank)
+    L_off_diag = random_state.randn(int(rank * (rank - 1) / 2))
+    L = log_chol_to_chol(torch.as_tensor(L_log_diag), torch.as_tensor(L_off_diag)).numpy()
+    return (L @ L.T) + eps * np.eye(rank)
+
+
+def _random_corr_mat(rank: int,
+                     eps: float = .001,
+                     random_state: Optional[np.random.RandomState] = None) -> np.ndarray:
+    cov = _random_cov_mat(rank, eps=eps, random_state=random_state)
+    _norm = np.diag(1 / np.sqrt(np.diag(cov)))
+    return _norm @ cov @ _norm
